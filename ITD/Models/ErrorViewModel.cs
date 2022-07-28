4a85cc7c97d91a0b27e8da944b1fc5a747e095eb
francisco <franciscodorset@hotmail.com>
2022-07-28 10:11:18 -0400

    first commit

---
+++ ITD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
